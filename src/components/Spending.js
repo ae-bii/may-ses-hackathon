563c09d978518e6faf6e2a42dd44000eb410379d
JenniferA <ayissi@usc.edu>
2022-05-19 15:50:30 -0400

    Added a header

--- src/components/Spending.js
+++ src/components/Spending.js
@@ -2,11 +2,22 @@ import React from 'react'
 
 const Spending = () => {
   return (
-    <div>Spending</div>
-    <h1>Annual Spending</h1>
+    <div>Spending
+        <h1>Recorded Spending Habits</h1>
+    </div>
   )
 }
+
 
 export default Spending
 
 //do npm start then i will be able to view the website
+/*
+1. git pull
+2. git checkout -b Jennifer/branchName
+    1. OR git pull origin main
+3. ~make changes~
+4. git add .
+5. git commit -m “note”
+6. git push origin Jennifer/branchName
+*/
