5fe0bfecaaf67c9ef5a1f5c55624c6674e0b0f1e
Bubble4096 <hillmile@usc.edu>
2022-05-20 03:22:39 -0400

    fixing budgets a bit

--- src/components/budgeting/Charts.js
+++ src/components/budgeting/Charts.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import BarChart from './babycomponent/BarChart.js'
-import './Budgeting.css'
-
+import './Charts.css'
 
 import Header from '../header/Header'
 const Charts  = () => {
 	return (
-	<div>
-		<h1>BarChart</h1>
+	<div className='ChartDesign'>
 		<BarChart
 		/>
 	</div>
--- src/components/budgeting/babycomponent/BarChart.js
+++ src/components/budgeting/babycomponent/BarChart.js
@@ -1,96 +1,100 @@
 import React from "react";
 import Chart from "react-apexcharts";
+import './BarChart.css'
 
 function Barchart() {
   return (
-    <React.Fragment>
-      <div className="container-fluid mb-5">
-        <Chart
-          type="bar"
-          width={1380}
-          height={700}
-          series={[
-              
-            {
-              name: "Money Spent",
-              data: [400, 300, 750, 350, 200,],
+    <div className="container-fluid1">
+      <React.Fragment>
+        <div className="container-fluid">
+          <Chart
+            type="bar"
+            width={1200}
+            height={700}
+            series={[
+                
+              {
+                name: "Money Spent",
+                data: [400, 300, 750, 350, 200,],
 
-            },
-          ]}
-
-          /* working on an overlay
-          
-          seriesb={[
-            <div className='chart2'>
-            {
-              name: "Budget",
-              data: [500, 500, 500, 500, 500,],
-
-            },
-          </div>
-          ]}*/
-
-          options={{
-            title: {
-              text: "June 2022",
-              style: { fontSize: 30 },
-            },
-
-          /*  subtitle: {
-              text: "This is BarChart Graph",
-              style: { fontSize: 18 },
-            },
-        */
-
-            colors: ["#86B2CE"],
-            theme: { mode: "light" },
-    
-            xaxis: {
-              tickPlacement: "on",
-              categories: [
-                "Groceries",
-                "Education",
-                "Food", 
-                "Fun",
-                "Other Expenses",
-              ],
-              title: {
-                text: "Budget Categories",
-                style: { color: "#004977", fontSize: 30 },
               },
-            },
+            ]}
 
-            yaxis: {
-              labels: {
+            /* working on an overlay
+            
+            seriesb={[
+              <div className='chart2'>
+              {
+                name: "Budget",
+                data: [500, 500, 500, 500, 500,],
+
+              },
+            </div>
+            ]}*/
+
+            options={{
+              title: {
+                text: "June 2022",
+                style: { fontSize: 30, textalign: 'center' },
+                
+              },
+
+            /*  subtitle: {
+                text: "This is BarChart Graph",
+                style: { fontSize: 18 },
+              },
+          */
+
+              colors: ["#86B2CE"],
+              theme: { mode: "light" },
+      
+              xaxis: {
+                tickPlacement: "on",
+                categories: [
+                  "Groceries",
+                  "Education",
+                  "Food", 
+                  "Fun",
+                  "Other Expenses",
+                ],
+                title: {
+                  text: "Budget Categories",
+                  style: { color: "#004977", fontSize: 30 },
+                },
+              },
+
+              yaxis: {
+                labels: {
+                  formatter: (val) => {
+                    return `${val}`;
+                  },
+                  style: { fontSize: "20", colors: ["#004977"] },
+                },
+                title: {
+                  text: "Cost($)",
+                  style: { color: "#004977", fontSize: 20 },
+                },
+              },
+
+              legend: {
+                show: true,
+                position: "right",
+              },
+
+              dataLabels: {
                 formatter: (val) => {
                   return `${val}`;
                 },
-                style: { fontSize: "20", colors: ["#004977"] },
+                style: {
+                  colors: ["#f4f4f4"],
+                  fontSize: 20,
+                },
               },
-              title: {
-                text: "Cost($)",
-                style: { color: "#004977", fontSize: 20 },
-              },
-            },
-
-            legend: {
-              show: true,
-              position: "right",
-            },
-
-            dataLabels: {
-              formatter: (val) => {
-                return `${val}`;
-              },
-              style: {
-                colors: ["#f4f4f4"],
-                fontSize: 20,
-              },
-            },
-          }}
-        ></Chart>
-      </div>
-    </React.Fragment>
+            }}
+          ></Chart>
+        </div>
+      </React.Fragment>
+    </div>
   );
 }
 
