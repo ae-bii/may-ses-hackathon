97a5ef664c69f263f7737d8c4e795e2afac2a8d2
Bubble4096 <hillmile@usc.edu>
2022-05-20 00:38:50 -0400

    this is it

--- src/components/budgeting/Budgeting.js
+++ src/components/budgeting/Budgeting.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import { BudgetingCard1 } from './budgetbutton'
+import { BudgetingCard2 } from './budgetbutton'
+import { BudgetingCard3 } from './budgetbutton'
+import { BudgetingCard4 } from './budgetbutton'
+import BarChart from './babycomponent/BarChart.js'
+import './Budgeting.css'
+
 
 const Budgeting = () => {
 	return (
-		<div>Budgeting</div>
-	)
+		<div>
+			<div>
+				<div className='budgetCards'>
+					<BudgetingCard1 />
+					<BudgetingCard2 />
+					<BudgetingCard3 />
+					<BudgetingCard4 />
+				</div>
+			</div>
+		</div>
+	)   
 }
 
-export default Budgeting
+const Charts  = () => {
+	return (
+	<div>
+		<h1>BarChart</h1>
+		<BarChart
+		/>
+	</div>
+	)   
+}
+export default Charts
